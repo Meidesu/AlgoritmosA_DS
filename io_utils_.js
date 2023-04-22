471c08f1630bcfe63d235778d4a1cb0165fda90c
Antonio Meireles <antonio.mei.nt@gmail.com>
2023-04-22 16:04:53 -0300

    io utils

---
+++ io_utils_.js
@@ -0,0 +1,31 @@
+import { question } from "readline-sync";
+
+export function input_num(cont){
+    let num = input(cont)
+
+    while (isNaN(Number(num)) || num === ''){
+        print('Insira um número válido!')
+        num = input(cont)
+    }
+
+    return Number(num)
+}
+
+export function input_num_positivo(cont='Insira um numero positivo: '){
+    let num = input_num(cont)
+
+    while (num <= 0 ){
+        print('Por favor insira um número positivo!')
+        num = input_num(cont)
+    }
+
+    return num
+}
+
+export function input(cont){
+    return question(cont)
+}
+
+export function print(cont){
+    return console.log(cont)
+}

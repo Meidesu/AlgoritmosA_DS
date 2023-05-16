d434e2152654b05f48bc0f11f3617d09f770606f
Meidesu <108002407+Meidesu@users.noreply.github.com>
2023-05-16 02:11:46 +0000

    "Beecrowd"

--- Beecrowd/bc_python/hello_bee/bc_1049_animal.py
+++ Beecrowd/bc_python/hello_bee/bc_1049_animal.py
@@ -1,19 +1,41 @@
 def main():
-  inicio, fim = map(int, input().split(' '))
 
-  tempo_de_jogo(inicio, fim)
+  carac_1  = 'invertebrado'
+  carac_2  = 'anelideo'
+  carac_3  = 'hematofago'
+  # carac_1  = input().split(' ')
+  # carac_2  = input().split(' ')
+  # carac_3  = input().split(' ')
 
-
-def tempo_de_jogo(inicio, fim):
+  definir_nome_do_animal(carac_1, carac_2, carac_3)
   
-  if inicio == fim:
-    print('O JOGO DUROU 24 HORA(S)')
-  elif inicio < fim :
-    duracao = fim - inicio
-    print(f'O JOGO DUROU { duracao } HORA(S)')
-  else:
-    duracao = ( 24 - inicio ) + fim
-    print(f'O JOGO DUROU { duracao } HORA(S)')
+
+
+def definir_nome_do_animal(carac_1, carac_2, carac_3):
+    if carac_1 == 'vertebrado':
+        if carac_2 == 'ave':
+            if carac_3 == 'carnivoro':
+                print('aguia')
+                
+            elif carac_3 == 'onivoro':
+                print('pomba')
+                
+        elif carac_2 == 'mamifero':
+            if carac_3 == 'onivoro':
+                print('homem')
+            elif carac_3 == 'herbivoro':
+                print('vaca')
+      
+    elif carac_1 == 'invertebrado':
+        if carac_2 == 'inseto':
+            if carac_3 == 'hematofago':
+                print('pulga')
+            else:
+                print('lagarta')
+        else:
+            if carac_3 == 'hematofago':
+                print('sanguessuga')
+            else:
+                print('minhoca')
 
 main()
-
